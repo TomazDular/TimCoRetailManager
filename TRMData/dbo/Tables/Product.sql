32a0515eb70ce02118cdeab4e822f664c5a34a3f
Tomaž Dular <tomaz.dular@b2-bi.com>
2021-08-08 12:09:41 +0200

    Register SQL Tables
    Register the basic SQL tables

---
+++ TRMData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Desctiption] NVARCHAR(MAX) NOT NULL, 
+    [RetailPrice] MONEY NOT NULL,
+    [CreateDate] DATETIME2 NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NULL DEFAULT getutcdate()
+)
